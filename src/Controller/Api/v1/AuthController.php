b9577e4cd87aa976fac0d193ecc4c2677467d778
Ainara Saracho <ainara.saracho@gmail.com>
2021-12-28 19:01:08 +0100

    Add JWT Autentication and AuthController with register endpoint

---
+++ src/Controller/Api/v1/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+* Código a partir de
+* https://h-benkachoud.medium.com/
+*/
+
+namespace App\Controller\Api\v1;
+
+ use App\Entity\User;
+ use Doctrine\Persistence\ManagerRegistry;
+ use FOS\RestBundle\Controller\Annotations as Rest;
+ use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+ use Symfony\Component\HttpFoundation\JsonResponse;
+ use Symfony\Component\HttpFoundation\Request;
+ use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+ use Symfony\Component\Security\Core\User\UserInterface;
+
+ class AuthController extends ApiController
+ {
+     /**
+      * Register user.
+      *
+      *@Rest\Post("/register")
+      *
+      *@return Response
+      */
+     public function register(Request $request, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher)
+     {
+         $em = $doctrine->getManager();
+         $request = $this->transformJsonBody($request);
+         $password = $request->get('password');
+         $email = $request->get('email');
+
+         if (empty($password) || empty($email)) {
+             return $this->respondValidationError('Invalid Username or Password or Email');
+         }
+
+         $user = new User($email);
+         $user->setPassword($passwordHasher->hashPassword($user, $password));
+         $user->setEmail($email);
+         $em->persist($user);
+         $em->flush();
+
+         return $this->respondWithSuccess(sprintf('User %s successfully created', $user->getUserIdentifier()));
+     }
+
+     /**
+      * @return JsonResponse
+      */
+     public function getTokenUser(UserInterface $user, JWTTokenManagerInterface $JWTManager)
+     {
+         return new JsonResponse(['token' => $JWTManager->create($user)]);
+     }
+ }
